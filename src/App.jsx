e15cd2a81ce1a6a57812ecacf78f921e9a75e6f4
Alireza Ghasemi <agh102724@gmail.com>
2024-12-24 23:45:12 +0330

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import React from "react";
+import Header from "./components/Header";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import logo from "../assets/images/logo.svg";
+import { FaFacebook, FaTwitter, FaSquarespace } from "react-icons/fa";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex flex-row justify-around bg-white py-5">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <nav>
+        <ul className="flex flex-row gap-5 text-[rgba(0,0,0,0.7)]">
+          <li>H o m e</li>
+          <li>A b o u t</li>
+          <li>S e r v i c e s</li>
+          <li>T o u r s</li>
+        </ul>
+      </nav>
+      <div className="icons flex flex-row gap-3 text-xl text-blue-400">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://x.com">
+          <FaTwitter />
+        </a>
+        <a href="https://squarespace.com">
+          <FaSquarespace />
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
